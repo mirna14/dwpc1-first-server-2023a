a8f0e8fcde3b5f7fae2efd4762e5571b26e2c56c
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-05-03 05:41:07 +0000

    ⭐Author

--- index.js
+++ index.js
@@ -1,21 +1,89 @@
 import http from "http";
 
-// 2. Crear el servidor 
-// cb (callback) es una *funcion* que se ejecutara 
-// ante cualquier peticion de un recurso a nuestro server 
-// (request, response) 
-
 const server = http.createServer((req, res) => {
-  console.log("> Se ha recibido una petición."); 
-  // Respondemos 
-  res.write("Hola");
-  // Se termina la conexión
-	res.end();
+   // Desestructurando de "req"
+   let { url, method } = req;
+
+console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
+
+// Enrutando peticiones
+switch (url) {
+  case '/':
+    // Peticion raiz
+    // Estableciendo cabeceras
+    res.setHeader('Content-Type', 'text/html');
+    // Escribiendo la respuesta
+    res.write(`
+    <html>
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+        <title>My App</title>
+      </head>
+      <body> 
+        <h1 style="color: #333">Hello from my server</h1>
+        <p style="color: #34495E">Estas en el recurso raiz.</p>
+      </body>
+    </html>
+    `);
+    console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+    // Estableciendo codigo de respuesta
+    res.statusCode = 200;
+    // Cerrando la comunicacion
+    res.end();
+    break;
+  default:
+    // Peticion raiz
+    // Estableciendo cabeceras
+    res.setHeader('Content-Type', 'text/html');
+    // Escribiendo la respuesta
+    res.write(`
+    <html>
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+        <title>My App</title>
+      </head>
+      <body> 
+        <h1>&#128534; 404 Recurso no encontrado</h1>
+        <p>Lo sentimos pero no tenemos lo que buscas...</p>
+      </body>
+    </html>
+    `);
+    console.log(`📣 Respondiendo: 404 ${req.url} ${req.method}`);
+    // Estableciendo codigo de respuesta
+    res.statusCode = 404;
+    // Cerrando la comunicacion
+    res.end();
+    break;
+    case '/author':
+      // Peticion raiz
+      // Estableciendo cabeceras
+      res.setHeader('Content-Type', 'text/html');
+      let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
+      // Escribiendo la respuesta
+      res.write(`
+      <html>
+        <head>
+          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <title>My App</title>
+        </head>
+        <body style="text-align: center;">
+          <h1 style="color: #333;">&#9889; Author &#9889;</h1>
+          <p style="color: #34495E;">Diaz Sanchez Mirna - Future Ing. Tics</p>
+          <div>
+          <img src="/img/mirna.jpg" width="400" height="341">
+          </div>
+        </body>
+      </html>
+      `);
+      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+      // Estableciendo codigo de respuesta
+      res.statusCode = 200;
+      // Cerrando la comunicacion
+      res.end();
+      break;
+}
 }); 
 
-// 3. Se pone a trabajar el servidor 
-// Se le pasa un callback que se ejecutara cunado
-// el servidor empiece a recibir peticiones 
 
 server.listen(3000, "0.0.0.0", () => {
   console.log("👩‍🍳 Servidor escuchando en http://localhost:3000"); 
